188c3d4bd91faf778b96745a6a0b02dc9a55bbbd
Peter <peterkim1984@gmail.com>
2014-06-18 22:02:58 -1000

    working body click code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 $(function(){
 
   for (var i = 0; i < window.geemails.length; i++) {
-    $("#emailList").append("<li> Subject : " + window.geemails[i].subject + " Sender : " + window.geemails[i].sender + "</li>");
+    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <b>Subject :</b> " + window.geemails[i].subject + " <b>Sender :</b> " + window.geemails[i].sender + "</li> <div id=\"email" + i + "\" class=\"emailBody\">" + window.geemails[i].body + "</div>");
+    $(".emailBody").hide();
+
+
   };
 
-setInterval(function(){}, 10000);
 
+$('ul li').click(function(){
+
+  var emailclicked = $(this).data('emailid')
+    
+  console.log(emailclicked)
+  $("#" + emailclicked).show();
+});
 
 });
