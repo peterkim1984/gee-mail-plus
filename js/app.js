2b468a01970b8bf473af9b765550a5b69f3e0c42
Peter <peterkim1984@gmail.com>
2014-06-19 19:35:05 -1000

    some organization, css and foundation styling, and added date

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
-$(function(){
+$(function() {
 
   for (var i = 0; i < window.geemails.length; i++) {
-    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <b>Subject :</b> " + window.geemails[i].subject + " <b>Sender :</b> " + window.geemails[i].sender + "</li> <div id=\"email" + i + "\" class=\"emailBody\">" + window.geemails[i].body + "</div>");
+    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <div class=\"large-4 columns\"><b>Date:</b> " + window.geemails[i].date + "</div><div class=\"large-3 columns\"><b>Sender:</b> " + window.geemails[i].sender + " </div><div class=\"large-5 columns\"><b>Subject:</b> " + window.geemails[i].subject + "</li> </div><div id=\"email" + i + "\" class=\"large-12 columns emailBody\">" + window.geemails[i].body + "</div>");
     $(".emailBody").hide();
 
 
   };
 
 
-$('ul li').click(function(){
+  $('ul li').click(function() {
 
-  var emailclicked = $(this).data('emailid')
-  if (!$("#" + emailclicked).hasClass("opened")){
+    var emailclicked = $(this).data('emailid')
+    if (!$("#" + emailclicked).hasClass("opened")) {
 
-  $("#" + emailclicked).show().addClass("opened");
-}
-else
-{
-  $("#" + emailclicked).hide().removeClass("opened");
-}
-});
+      $("#" + emailclicked).show().addClass("opened");
+    } else {
+      $("#" + emailclicked).hide().removeClass("opened");
+    }
+  });
 
 });
