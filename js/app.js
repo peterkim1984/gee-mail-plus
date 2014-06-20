b90f4fb7aeda3a098323d69f32ea06e3ee4a0f88
Peter <peterkim1984@gmail.com>
2014-06-19 20:19:54 -1000

    removed old flappy app files, made line, made geemail logo and added to page.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 
   for (var i = 0; i < window.geemails.length; i++) {
-    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <div class=\"large-4 columns\"><b>Date:</b> " + window.geemails[i].date + "</div><div class=\"large-3 columns\"><b>Sender:</b> " + window.geemails[i].sender + " </div><div class=\"large-5 columns\"><b>Subject:</b> " + window.geemails[i].subject + "</li> </div><div id=\"email" + i + "\" class=\"large-12 columns emailBody\">" + window.geemails[i].body + "</div>");
+    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <div class=\"large-4 columns\"><b>Date:</b> " + window.geemails[i].date + "</div><div class=\"large-3 columns\"><b>Sender:</b> " + window.geemails[i].sender + " </div><div class=\"large-5 columns\"><b>Subject:</b> " + window.geemails[i].subject + " </div><div id=\"email" + i + "\" class=\"large-12 columns emailBody\">" + window.geemails[i].body + "</div></li>");
     $(".emailBody").hide();
 
 
