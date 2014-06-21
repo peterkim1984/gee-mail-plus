017982598e7301f119c451aaedb50025e4554411
Peter <peterkim1984@gmail.com>
2014-06-21 11:56:25 -1000

    everything seems functional

--- js/app.js
+++ js/app.js
@@ -1,22 +1,54 @@
 $(function() {
 
-  for (var i = 0; i < window.geemails.length; i++) {
-    $("#emailList").append("<li data-emailid=\"email" + i + "\"> <div class=\"large-4 columns\"><b>Date:</b> " + window.geemails[i].date + "</div><div class=\"large-3 columns\"><b>Sender:</b> " + window.geemails[i].sender + " </div><div class=\"large-5 columns\"><b>Subject:</b> " + window.geemails[i].subject + " </div><div id=\"email" + i + "\" class=\"large-12 columns emailBody\">" + window.geemails[i].body + "</div></li>");
-    $(".emailBody").hide();
+  var inbox = [];
 
+  for (var i = 0; i < window.geemails.length; i++) {
+    inbox.push(window.geemails[i]);
+    $("#emailList").append("<li data-emailid=\"email" + inbox.length +
+      "\"> <div class=\"large-4 columns\"><b>Date:</b> " + window.geemails[i].date +
+      "</div><div class=\"large-3 columns\"><b>Sender:</b> " + window.geemails[i].sender +
+      " </div><div class=\"large-5 columns\"><b>Subject:</b> " + window.geemails[i].subject +
+      " </div><div id=\"email" + i + "\" class=\"large-12 columns emailBody\">" + window.geemails[i].body +
+      "</div></li>");
+
+    $(".emailBody").hide();
+    $("#mailcount").html("Inbox (" + inbox.length + ")")
 
   };
 
 
-  $('ul li').click(function() {
 
-    var emailclicked = $(this).data('emailid')
-    if (!$("#" + emailclicked).hasClass("opened")) {
+  setInterval(displayNewMessages, 5000);
 
-      $("#" + emailclicked).show().addClass("opened");
-    } else {
-      $("#" + emailclicked).hide().removeClass("opened");
-    }
+
+  function displayNewMessages() {
+    var newMessage = getNewMessage();
+    inbox.push(newMessage);
+    $("#emailList").prepend("<li data-emailid=\"email" + inbox.length +
+      "\"> <div class=\"large-4 columns\"><b>Date:</b> " + newMessage.date +
+      "</div><div class=\"large-3 columns\"><b>Sender:</b> " + newMessage.sender +
+      " </div><div class=\"large-5 columns\"><b>Subject:</b> " + newMessage.subject +
+      " </div><div id=\"email" + inbox.length + "\" class=\"large-12 columns emailBody\">" + newMessage.body +
+      "</div></li>");
+    $("#email" + inbox.length).hide();
+    $("#mailcount").html("Inbox (" + inbox.length + ")")
+    // $('ul li').click(function() {
+
+    //   var emailclicked = $(this).data('emailid');
+    //   $(".emailBody").hide();
+    //   $(this).find(".emailBody").show();
+    // });
+
+
+
+  }
+
+  $('ul').on("click", "li",function() {
+
+    var emailclicked = $(this).data('emailid');
+    $(".emailBody").hide();
+    $(this).find(".emailBody").show();
   });
+
 
 });
